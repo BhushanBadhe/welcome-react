9becb6906fff9da613745d63dfad5ed765593896
bhushanbadhe <badhe.bhushan@mahindra.com>
2023-08-17 21:06:33 +0530

    Functional Components and  JSX Covered

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-
-
-{/* <div id="parent">
+import React from "react";
+import ReactDOM from "react-dom/client";
+{
+  /* <div id="parent">
     <div id="child">
         <h1></h1>
     </div>
-</div> */}
+</div> */
+}
 
+// const parent = React.createElement("div",{id:"parent"},
+//     React.createElement("div",{id:"child"},
+//     [React.createElement("h1",{},"I'm an h1 tag"),React.createElement("h2",{},"I'm an h2 tag")]
+//     )
+// )
 
-const parent = React.createElement("div",{id:"parent"},
-    React.createElement("div",{id:"child"},
-    [React.createElement("h1",{},"I'm an h1 tag"),React.createElement("h2",{},"I'm an h2 tag")]
-    )
-)
+// JSX ==> Javascript XML
+const JsxHeadingElement = "jsxHeading is title of this topic";
 
+// React Component
+// Class Based Components
 
-const heading = React.createElement('h1',{id:'heading'},"React is here");
-console.log(heading);
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+// Functional Component ===> It's latest way to code in react. It's just a function. Just name the functional component with capital letter otherwise you'll get error
+
+const TitleComponent = () =>  <h1>TitleComponent here</h1>
+const spanTag = <span>Span Tag here</span>
+const HeaderComponent = () => (
+  <div className="container">
+    {spanTag}
+    {TitleComponent()}
+    <h1>{JsxHeadingElement}</h1>
+    <h1 id="heading">Header</h1>
+  </div>
+);
+
+const heading = React.createElement("h1", { id: "heading" }, "React is here");
+// console.log(jsxHeading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeaderComponent />);
--- Readme.md
+++ Readme.md
@@ -1,3 +1,4 @@
+Episode 1 - 
 React Element  = Javascript Object  ===> Converted into HTML ==>  Which is understood by browser.
 
 
@@ -11,4 +12,40 @@ childrent ==> What should the content into the React element.
 
 
 ReactDOM has createRoot method to create root element for manipulating HTML page using virtual DOM. 
-ReactDOM has render method used to render react element onto Index.html
+ReactDOM has render method used to render react element onto Index.html.
+
+
+Session(Episode) 2 -
+1. npm is a standard repository of node packages. 
+
+npm init command is used to create package.json file 
+
+package.json file is dependency file which contains standard node packages. 
+
+Dependencies are most important package (called bundler). 
+
+bundler basically bundles our app means packages our app so we can shift our app to production. 
+
+webpack, parcel and vite are the example of bundlers. 
+
+
+To install dependency we should hit command ==> npm install parcel. 
+
+
+
+*=====> JSX <======*
+JSX stands for javascript and XML 
+JSX made our life easy by replacing React.createElement functionality. 
+
+Eg 
+const jsxHeading = <h1>JSX Heading here </h1>
+
+if we want to implement JSX  dynamically or run JS code then we need to wrap it into curly braces. 
+
+3 ways to call component using JSX 
+Eg 
+const TitleComponent = () => <p>Title Component here</p>
+
+1. {TitleComponent()}
+2. <TitleComponent/>
+3. <TitleComponent><TitleComponent/>
